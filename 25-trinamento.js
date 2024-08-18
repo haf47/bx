476fab0370c41fab639ea2a89865150006e43bf8
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-08-18 09:38:54 -0300

    Add files via upload

---
+++ 25-trinamento.js
@@ -0,0 +1,21 @@
+console.log ("Treinando com if,else.");
+
+const maiorIdade = 10;
+console.log (maiorIdade);
+
+
+if (maiorIdade >= 18) {
+
+    console.log ("Ok,passagem liberada.");
+    console.log("Tudo certo, dirija-se ao portão.");
+    
+} else {
+
+    console.log ("Voce é menor, não pode embarcar.");
+    console.log("Embarque proibido por lei.");
+    console.log("Você precisa retornar ao balcão com um resposável legal.");
+}
+   
+
+
+
